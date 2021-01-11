3c694de1e24784dcde73c03e6826d2e08759fae9
Mauro Oviedo <mauro.oviedo@protonmail.com>
2021-01-11 16:34:14 -0500

    Fixed cast call.

--- lib/alchemy_book/user.ex
+++ lib/alchemy_book/user.ex
@@ -15,7 +15,7 @@ defmodule AlchemyBook.User do
 
   def changeset(model, params \\ :invalid) do
     model
-    |> cast(params, ~w(name username))
+    |> cast(params, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
   end
